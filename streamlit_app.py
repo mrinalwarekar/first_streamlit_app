5f57e6d94264c8ae1cfe277d3230a52926c1e69c
mrinalwarekar <68075522+mrinalwarekar@users.noreply.github.com>
2022-07-30 21:42:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('my parensts new healthy codde')
 streamlit.title('new code codde')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
